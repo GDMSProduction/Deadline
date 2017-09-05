1a3e12acda5dc8c1e6251e9acfdd637bbd8a0010
Legend2021 <leonwilson.a28@gmail.com>
2017-09-05 17:16:40 -0400

    Connected the create account screen to the homepage

--- Deadline/app/src/main/java/com/example/leon/deadline/CreateAccount.java
+++ Deadline/app/src/main/java/com/example/leon/deadline/CreateAccount.java
@@ -1,13 +1,17 @@
 package com.example.leon.deadline;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
 
 public class CreateAccount extends AppCompatActivity {
+
+    private Button create1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,6 +19,25 @@ public class CreateAccount extends AppCompatActivity {
         setContentView(R.layout.activity_create_account);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        create1 = (Button) findViewById(R.id.finalizeCreate);
+        create1.setOnClickListener(new View.OnClickListener()
+                                  {
+                                      @Override
+                                      public void onClick(View v)
+                                      {
+                                          /*eEmail = (EditText) findViewById(R.id.email);
+                                          ePass = (EditText) findViewById(R.id.password);
+
+                                          email = eEmail.getText().toString();
+                                          pass = ePass.getText().toString();
+
+                                          createAccount(email,pass);*/
+                                          Intent intent = new Intent(CreateAccount.this, HomeScreen.class);
+                                          startActivity(intent);
+                                      }
+                                  }
+        );
     }
 
 }
