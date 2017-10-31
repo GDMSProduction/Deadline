d51dd5ab33606bdb7a2ba417199c3ba6975f19d4
Alex Pinner <alex.pinner@yahoo.com>
2017-10-31 19:27:26 -0400

    Added edit settings screen

---
+++ Deadline/app/src/main/java/com/example/leon/deadline/EditTask.java
@@ -0,0 +1,33 @@
+package com.example.leon.deadline;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.content.Intent;
+import android.widget.Button;
+
+public class EditTask extends AppCompatActivity {
+    private Button Butt_Home;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_task);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        Butt_Home = (Button) findViewById(R.id.Home_Button);
+        Butt_Home.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(EditTask.this, HomeScreen.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+
+}
