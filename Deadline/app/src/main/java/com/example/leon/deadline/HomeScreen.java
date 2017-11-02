1e4aae8f22f9f7200936b0102a0a6c7211ce71cb
sml91490 <sml91490@aol.com>
2017-11-02 18:14:21 -0400

    -Fixed Firebase Crash on Homescreen

--- Deadline/app/src/main/java/com/example/leon/deadline/HomeScreen.java
+++ Deadline/app/src/main/java/com/example/leon/deadline/HomeScreen.java
@@ -172,7 +172,7 @@ public class HomeScreen extends AppCompatActivity {
                 {
                     if(mtest != null)
                     {
-                        CProject temp = new CProject(mtest.child("m_szName").getValue().toString(), mtest.child("m_Deadline").getValue().toString(), (Boolean) mtest.child("m_bPrivate").getValue());
+                        CProject temp = new CProject(mtest.child("name").getValue().toString(), mtest.child("deadline").getValue().toString(), (Boolean) mtest.child("bPrivate").getValue());
                         aTest[i] = temp;
                         i++;
                     }
@@ -188,7 +188,7 @@ public class HomeScreen extends AppCompatActivity {
                 int i = 0;
                 for(DataSnapshot mtest :dataSnapshot.getChildren())
                 {
-                    CProject temp = new CProject(mtest.child("m_szName").getValue().toString(), mtest.child("m_Deadline").getValue().toString(), (Boolean) mtest.child("m_bPrivate").getValue());
+                    CProject temp = new CProject(mtest.child("name").getValue().toString(), mtest.child("deadline").getValue().toString(), (Boolean) mtest.child("bPrivate").getValue());
                     aTest[i] = temp;
                     i++;
                 }
