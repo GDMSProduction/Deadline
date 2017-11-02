63acd1049588dee8fd2d2e03b24e90ab263245c1
Alex Pinner <alex.pinner@yahoo.com>
2017-11-02 17:08:43 -0400

    Fixed button names and intent switching

--- Deadline/app/src/main/java/com/example/leon/deadline/Projects.java
+++ Deadline/app/src/main/java/com/example/leon/deadline/Projects.java
@@ -3,7 +3,6 @@ package com.example.leon.deadline;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
@@ -24,7 +23,7 @@ public class Projects extends AppCompatActivity {
         CProject tempProject = new CProject();
         tempProject.setName("My Project");
 
-        Create_Project = (FloatingActionButton) findViewById(R.id.TaskCreate11);
+        Create_Project = (FloatingActionButton) findViewById(R.id.CreateProject);
         Create_Project.setOnClickListener(new View.OnClickListener()
         {
 
