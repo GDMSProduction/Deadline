00a9d055fc896fbafeef56f43bef0db921c68925
Alex Pinner <alex.pinner@yahoo.com>
2017-11-14 18:30:02 -0500

    Added file manager activity

---
+++ Deadline/app/src/main/java/com/example/leon/deadline/FileManager.java
@@ -0,0 +1,90 @@
+package com.example.leon.deadline;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.content.Intent;
+import android.widget.Button;
+import com.google.firebase.auth.FirebaseAuth;
+import android.widget.AdapterView;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+import java.io.File;
+
+public class FileManager extends AppCompatActivity {
+
+    private Button Butt_Home;
+
+    private FirebaseAuth mAuth;
+
+    private Spinner nav_spin;
+    private Boolean spin_Clicked = false;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_file_manager);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        Butt_Home = (Button) findViewById(R.id.Home_Button);
+        Butt_Home.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(FileManager.this, HomeScreen.class);
+                startActivity(intent);
+            }
+        });
+
+        mAuth = FirebaseAuth.getInstance();
+
+        nav_spin = (Spinner) findViewById(R.id.nav_Spinner);
+        nav_spin.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                String selection = parent.getSelectedItem().toString();
+                if (selection.equals("Projects") && spin_Clicked){
+                    Intent intent = new Intent(FileManager.this, Projects.class);
+                    nav_spin.setSelection(0);
+                    startActivity(intent);
+                }
+                else if (selection.equals("Settings") && spin_Clicked){
+                    Intent intent = new Intent(FileManager.this, Settings.class);
+                    nav_spin.setSelection(0);
+                    startActivity(intent);
+                }
+                else if (selection.equals("Account") && spin_Clicked){
+                    Intent intent = new Intent(FileManager.this, AccountInfo.class);
+                    nav_spin.setSelection(0);
+                    startActivity(intent);
+                }
+                else if (selection.equals("About") && spin_Clicked){
+                    Toast.makeText(FileManager.this, "Version: 171109_P3", Toast.LENGTH_SHORT).show();
+                    nav_spin.setSelection(0);
+                }
+                else if (selection.equals("Logout") && spin_Clicked){
+                    mAuth.signOut();
+                    Intent intent = new Intent(FileManager.this, Login.class);
+                    nav_spin.setSelection(0);
+                    startActivity(intent);
+                }
+                spin_Clicked = true;
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        nav_spin.setSelection(0);
+    }
+}
