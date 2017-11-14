c2179a63ad95c69a6538375548e15b418545e67b
Alex Pinner <alex.pinner@yahoo.com>
2017-11-14 16:35:57 -0500

    Added edit roles activity and added nav spinner to two more activities

--- Deadline/app/src/main/java/com/example/leon/deadline/EditRoles.java
+++ Deadline/app/src/main/java/com/example/leon/deadline/EditRoles.java
@@ -1,32 +1,31 @@
 package com.example.leon.deadline;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.content.Intent;
 import android.widget.Button;
+import com.google.firebase.auth.FirebaseAuth;
 import android.widget.AdapterView;
 import android.widget.Spinner;
 import android.widget.Toast;
 
-import com.google.firebase.auth.FirebaseAuth;
-
-public class CreateEditRoles extends AppCompatActivity {
+public class EditRoles extends AppCompatActivity {
 
     private FirebaseAuth mAuth;
+
+    private Button Butt_Home;
 
     private Spinner nav_spin;
     private Boolean spin_Clicked = false;
 
-    private Button Butt_Home;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_create_edit_roles);
+        setContentView(R.layout.activity_edit_roles);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -34,11 +33,10 @@ public class CreateEditRoles extends AppCompatActivity {
         Butt_Home.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(CreateEditRoles.this, HomeScreen.class);
+                Intent intent = new Intent(EditRoles.this, HomeScreen.class);
                 startActivity(intent);
             }
         });
-
 
         mAuth = FirebaseAuth.getInstance();
 
@@ -48,27 +46,27 @@ public class CreateEditRoles extends AppCompatActivity {
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
                 String selection = parent.getSelectedItem().toString();
                 if (selection.equals("Projects") && spin_Clicked){
-                    Intent intent = new Intent(CreateEditRoles.this, Projects.class);
+                    Intent intent = new Intent(EditRoles.this, Projects.class);
                     nav_spin.setSelection(0);
                     startActivity(intent);
                 }
                 else if (selection.equals("Settings") && spin_Clicked){
-                    Intent intent = new Intent(CreateEditRoles.this, Settings.class);
+                    Intent intent = new Intent(EditRoles.this, Settings.class);
                     nav_spin.setSelection(0);
                     startActivity(intent);
                 }
                 else if (selection.equals("Account") && spin_Clicked){
-                    Intent intent = new Intent(CreateEditRoles.this, AccountInfo.class);
+                    Intent intent = new Intent(EditRoles.this, AccountInfo.class);
                     nav_spin.setSelection(0);
                     startActivity(intent);
                 }
                 else if (selection.equals("About") && spin_Clicked){
-                    Toast.makeText(CreateEditRoles.this, "Version: 171109_P3", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(EditRoles.this, "Version: 171109_P3", Toast.LENGTH_SHORT).show();
                     nav_spin.setSelection(0);
                 }
                 else if (selection.equals("Logout") && spin_Clicked){
                     mAuth.signOut();
-                    Intent intent = new Intent(CreateEditRoles.this, Login.class);
+                    Intent intent = new Intent(EditRoles.this, Login.class);
                     nav_spin.setSelection(0);
                     startActivity(intent);
                 }
@@ -80,7 +78,6 @@ public class CreateEditRoles extends AppCompatActivity {
 
             }
         });
-
     }
 
     @Override
