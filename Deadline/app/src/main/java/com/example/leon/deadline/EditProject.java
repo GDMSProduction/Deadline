07c0944de7eed0ac9331cbd6a911419df6dae276
Alex Pinner <alex.pinner@yahoo.com>
2017-11-16 10:55:04 -0500

    modified some things, if the project breaks it was likely this commit

--- Deadline/app/src/main/java/com/example/leon/deadline/EditProject.java
+++ Deadline/app/src/main/java/com/example/leon/deadline/EditProject.java
@@ -32,7 +32,7 @@ public class EditProject extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        
+
 
         Butt_Home = (Button) findViewById(R.id.Home_Button);
         Butt_Home.setOnClickListener(new View.OnClickListener() {
