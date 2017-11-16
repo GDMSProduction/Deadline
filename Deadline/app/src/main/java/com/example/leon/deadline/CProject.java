485bfe0510a69eff9d789cc8c3c38feeeb08d12b
Legend2021 <leonwilson.a28@gmail.com>
2017-11-16 05:56:26 -0500

    Added clicking to custom adapter

--- Deadline/app/src/main/java/com/example/leon/deadline/CProject.java
+++ Deadline/app/src/main/java/com/example/leon/deadline/CProject.java
@@ -8,7 +8,7 @@ import java.util.Date;
 import java.util.List;
 
 public class CProject extends CDeadline implements Serializable {
-    
+
     private static int typeID = 0;
 
     private List<CTask> m_TaskList = new ArrayList<>();
